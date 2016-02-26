1d82ae3e15cef1041b5b1cff7e7ee6fb32a889c7
xvw <xavier.vdw@gmail.com>
2016-02-26 23:24:15 +0100

    Append Lift implementation

--- lib/olmiMake.ml
+++ lib/olmiMake.ml
@@ -18,3 +18,80 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
 *)
+
+open OlmiInterfaces
+
+let id x = x
+let flip f x y = f y x
+
+(* Functors for monad implementation *)
+(* Using join interface *)
+module WithJoin (M : JOIN) : BASIC_INTERFACE with type 'a t = 'a M.t =
+struct
+  include M
+  let bind m f = join (fmap f m)
+end
+
+
+(* Using bind interface *)
+module WithBind (M : BIND) : BASIC_INTERFACE with type 'a t = 'a M.t =
+struct
+  include M
+  let join m = bind m id
+  let fmap f m = bind m (fun x -> return (f x))
+end
+
+(* Functor for creating a complete monad *)
+module Monad (M : BASIC_INTERFACE) : INTERFACE with type 'a t = 'a M.t =
+struct
+
+  include M
+
+  let ( >>= ) = bind
+  let ( >|= ) x f = fmap f x
+  let ( >> ) m n = m >>= (fun _ -> n)
+
+  let ( <=< ) f g = fun x -> g x >>= f
+  let ( >=> ) f g = flip ( <=< ) f g
+  let ( =<< ) f x = flip ( >>= ) f x
+
+  let ( <*> ) fs ms =
+    fs >>= fun f ->
+    ms >>= fun x -> return (f x)
+
+  let ( *> ) x = ( <*> ) (fmap (fun _ -> id) x)
+  let ( <* ) x _ = ( <*> ) (return (fun x -> x)) x
+  let ( <**> ) f x = flip ( <*> ) f x
+
+  let ( <$> ) = fmap
+  let ( <$ ) v = fmap (fun _ -> v)
+
+  let liftM = fmap
+
+  let liftM2 f m1 m2 =
+    m1 >>= fun x ->
+    m2 >>= fun y -> return (f x y)
+
+  let liftM3 f m1 m2 m3 =
+    m1 >>= fun x ->
+    m2 >>= fun y ->
+    m3 >>= fun z -> return (f x y z)
+
+  let liftM4 f m1 m2 m3 m4 =
+    m1 >>= fun x ->
+    m2 >>= fun y ->
+    m3 >>= fun z ->
+    m4 >>= fun a -> return (f x y z a)
+
+  let liftM5 f m1 m2 m3 m4 m5 =
+    m1 >>= fun x ->
+    m2 >>= fun y ->
+    m3 >>= fun z ->
+    m4 >>= fun a ->
+    m5 >>= fun b -> return (f x y z a b)
+
+
+  let void _ = return ()
+  let forever x = let rec a = x >> a in a
+
+end
